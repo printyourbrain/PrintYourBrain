8dcb831cef60bb3387be7aafcfe8bc28559e48e9
anujanegi <anujanegi1@gmail.com>
2022-01-19 17:26:12 +0100

    ANTS brain extraction

--- pyb/reconstruction.py
+++ pyb/reconstruction.py
@@ -1,6 +1,10 @@
 """ Utils file for cortical surface reconstruction using freesurfer"""
 
 from nipype.interfaces.freesurfer import ReconAll
+from nipype.interfaces.ants.segmentation import BrainExtraction
+
+from config import template
+
 # >>> reconall = ReconAll()
 # >>> reconall.inputs.subject_id = 'foo'
 # >>> reconall.inputs.directive = 'all'
@@ -14,7 +18,7 @@ from nipype.interfaces.freesurfer import ReconAll
 # recon-all -skullstrip -clean-bm -gcut -subjid s9400_MS
 # recon-all -s s9400_MS -autorecon2 -autorecon3
 
-def autorecon1(subject, subject_directory, inputfile):
+def autorecon1(subject, subject_directory, T1file):
     """Performs the follwing operations-
         Motion Correction and Conform
         NU (Non-Uniform intensity normalization)
@@ -26,7 +30,7 @@ def autorecon1(subject, subject_directory, inputfile):
     autorecon1.inputs.subject_id = subject
     autorecon1.inputs.directive = 'autorecon1'
     autorecon1.inputs.subjects_dir = subject_directory
-    autorecon1.inputs.T1_files = inputfile
+    autorecon1.inputs.T1_files = T1file
     print(autorecon1.cmdline)
     autorecon1.run()
     
@@ -35,5 +39,34 @@ def recon_flow():
     """Overall recon pipeline"""
     autorecon1('exampleT1', 'data/exampleT1', 'data/exampleT1.nii')
     
-# autorecon1()
-recon_flow()
+def brain_extraction(T1file, template_name='NKI'):
+    """Performs Atlas based brain extraction with ANTS tool.
+    """
+    brainextraction = BrainExtraction()
+    brainextraction.inputs.dimension = 3
+    brainextraction.inputs.anatomical_image = T1file
+    brainextraction.inputs.brain_template = template[str(template_name)]['brain_template']
+    brainextraction.inputs.brain_probability_mask = template[str(template_name)]['probablity_mask']
+    brainextraction.inputs.args.o = 'dsd'
+    print(brainextraction.cmdline)
+    # brainextraction.run()
+
+# antsBrainExtraction.sh -a T1.nii.gz -m ProbabilityMaskOfStudyTemplate.nii.gz
+# -e study_template.nii.gz -d 3 -s nii.gz -o highres001_'
+
+# SUBJECT_DIR=""
+# T1_NAME="s9400-HR.nii"
+# TEMPLATE_DIR="" # folder containing the downloaded templates
+
+# BRAIN_TEMPLATE="T_template2.nii.gz" # anatomical template with skull
+# PROBABILITY_MASK="T_template_BrainCerebellumProbabilityMask.nii.gz" # make sure to use the one with cerebellum
+# OUTPUT_DIR=${SUBJECT_DIR}/output
+
+# antsBrainExtraction.sh \
+#   -a ${SUBJECT_DIR}/${T1_NAME} \
+#   -e ${TEMPLATE_DIR}/${TEMPLATE_NAME}/${BRAIN_TEMPLATE} \
+#   -m ${TEMPLATE_DIR}/${TEMPLATE_NAME}/${PROBABILITY_MASK} \
+#   -d 3 \
+#   -o ${OUTPUT_DIR}
+
+brain_extraction('data/exampleT1.nii')
